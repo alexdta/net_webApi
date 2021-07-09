1ee44ddde0f35a131c8622146ffba7265cf9b116
edgar <eacm83@gmail.com>
2021-07-09 02:03:44 -0600

    Changing from a list of strings to a Recipe's (class) List

--- net_api/Controllers/RecipesController.cs
+++ net_api/Controllers/RecipesController.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using net_api.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace net_api.Controllers
 {
@@ -12,7 +11,11 @@ namespace net_api.Controllers
     [ApiController]
     public class RecipesController : ControllerBase
     {
-        public static List<string> recipes = new List<string>(new string[] { "Oxtail", "Curry Chicken", "Dumplings" });
+        public static List<Recipe> recipes = new List<Recipe>() {
+            new () { Title = "Oxtail", Updated = new DateTime(2021,1,15)},
+            new () { Title = "Curry Chicken", Updated = new DateTime(2021,5,19)},
+            new () { Title = "Dumplings", Updated = DateTime.Now},
+        };
 
         [HttpGet("get_all")]
         public ActionResult GetDishes()
@@ -32,30 +35,54 @@ namespace net_api.Controllers
             return Ok(recipes[index]);
         }
 
-        [HttpPost("add/{recipe}")]
-        public ActionResult CreateNewRecipe(string recipe)
+        [HttpPost("add")]
+        public ActionResult CreateNewRecipe([FromBody] Recipe recipe)
         {
             recipes.Add(recipe);
 
-            return Created("", recipe);
+            return Created("", recipe.Title);
         }
+        /*
+        {
+          "title": "Gallo Pinto",
+          "description": "Typical Costa Rican recipe",
+          "directions": [
+            "to be defined . . ."
+          ],
+          "ingredients": [
+            "Rice", "Beans", "Lizano"
+          ],
+          "updated": "1900-01-01"
+        }         
+         */
 
-        [HttpPut("update/{index}/{value}")]
-        public ActionResult UpdateRecipe(int index, string value)
+        [HttpPut("update/{index}")]
+        public ActionResult UpdateRecipe(int index, [FromBody] Recipe recipe)
         {
             if (recipes.ElementAtOrDefault(index) == null)
                 return NotFound();
 
-            recipes[index] = value;
+            recipes[index] = recipe;
 
             return NoContent();
         }
+        /*
+        {
+	        "title": "Oxtail",
+	        "description": "",
+	        "directions": null,
+	        "ingredients": [
+		        "Chopped Oxtail", "Leeks", "Celery", "Carrots"
+	        ],
+	        "updated": "2021-07-08"
+        } 
+         */
 
         [HttpDelete("delete/{index}")]
         public ActionResult DeleteRecipes(int index)
         {
-           if (recipes.ElementAtOrDefault(index) == null)
-                return BadRequest();                
+            if (recipes.ElementAtOrDefault(index) == null)
+                return BadRequest();
 
             recipes.RemoveAt(index);
 
---
+++ net_api/Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace net_api.Models
+{
+    public record Recipe
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public IEnumerable<string> Directions { get; set; }
+
+        public IEnumerable<string> Ingredients { get; set; }
+
+        public DateTime Updated { get; set; }
+
+    }
+}
